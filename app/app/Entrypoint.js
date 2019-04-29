b738ebfd00da6dcbb9ef077b1341c372807a3e08
Victor Arnaud <victorhad@gmail.com>
2019-04-28 23:14:12 -0300

    Reorganizando os projeto

---
+++ app/app/Entrypoint.js
@@ -0,0 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React from 'react';
+import NumerosRandomicos from './NumerosRandomicos';
+
+const App = () => <NumerosRandomicos />;
+
+export default App;
--- app/app/NumerosRandomicos/index.js
+++ app/app/NumerosRandomicos/index.js
@@ -1,13 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { styles } from './style';
 
-export default class App extends Component {
+export default class NumerosRandomicos extends Component {
   render() {
     return (
       <View style={styles.container}>
